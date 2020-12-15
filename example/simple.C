53885025370720b740d04ac4311c9b6c0636a976
Valerio Mascagna <vmascagn@ifarm1802.jlab.org>
2020-12-15 16:14:21 -0500

    simple.C simplified... again

--- example/simple.C
+++ example/simple.C
@@ -1,18 +1,14 @@
 void simple(){
 	
-	// create a Hipo chain with multiple files
-	
-	HipoChain chain;
-	chain.Add('./skim2_005339_first1000.hipo');
-	chain.Add('./skim2_005343_first1000.hipo');
+  // create a clas12reader
+  clas12reader c12("./skim2_005339_first1000.hipo"); 
 
+  while(c12.next()) { //loop over events
 
-	auto config_c12=chain.GetC12Reader(); //in case you want to configure, use this
-  	auto& c12=chain.C12ref();
-  	while(chain.Next()==true){
+    auto particles = c12.getDetParticles(); //particles is now a std::vector of particles for this event
 
-	    auto particles = c12->getDetParticles();
-    	std::cout << particles.size() << std::endl;
-	}
+    //If you just want to loop over all particles, best to use a c++11 range based for loop
+    std::cout << particles.size() << std::endl;
 
+  }
 }
